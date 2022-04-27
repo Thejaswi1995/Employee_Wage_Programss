d4b998ee026aa0a3ade490a96e3d18ecf62ccef2
Thejaswi <thejaswirl187@gmail.com>
2022-04-27 18:20:16 +0530

    Added : Calculation of fulltime and part time-UC_3

--- src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
+++ src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
@@ -2,14 +2,19 @@ package com.bridgelabz.employeewage;
 public class EmployeeWagePrograms {
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOUR = 8;
+    private static final int HALF_DAY_HOUR = 4;
 
     public static void main(String[] args) {
         int dailyWage = 0;
         double x = Math.random();
-        int isPresent = (int) (x * 10) % 2;
-        if (isPresent == 0) {
-            System.out.println("Employee is present");
+        int empStatus = (int) (x * 10) % 3;
+        if (empStatus == 1) {
+            System.out.println("Employee worked full time");
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        }
+            else if (empStatus == 2) {
+                System.out.println("Employee worked part time");
+                dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
         } else {
             System.out.println("Employee is Absent");
         }
