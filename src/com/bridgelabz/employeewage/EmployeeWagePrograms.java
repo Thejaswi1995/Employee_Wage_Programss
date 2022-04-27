e230ce7322a1ba1ac2179010b0cf1b07eb121f6c
Thejaswi <thejaswirl187@gmail.com>
2022-04-27 19:36:56 +0530

    Added : Calculation Wages till a condition of total working hours or days reached for month_UC-6

--- src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
+++ src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
@@ -1,37 +1,51 @@
 package com.bridgelabz.employeewage;
 public class EmployeeWagePrograms {
     private static final int WAGE_PER_HOUR = 20;
-    private static final int FULL_DAY_HOUR = 8;
-    private static final int HALF_DAY_HOUR = 4;
-    private static final int IS_FULLTIME = 1;
-    private static final int IS_HALFTIME = 2;
+    private static final int FULL_DAY_HOUR = 10;
+
     private static final int TOTAL_WORKING_DAYS = 20;
 
+    private static final int TOTAL_WORKING_HOURS = 100;
 
+    private static final int HALF_DAY_HOUR = 5;
+
+    private static final int IS_FULL_TIME = 1;
+
+    private static final int IS_PART_TIME = 2;
+
+    // Naming conventions: class and interfaces => Pascal case(eg: HelloWorld)
+// methods and variables : camel case(eg: instanceVar)
+// constants(final keyword) : Upper-case with underscore (eg: WORKING_HOURS)
     public static void main(String[] args) {
-        int dailyWage = 0;
-        int dayCount = 1;
-        while (dayCount <= TOTAL_WORKING_DAYS) {
 
-            double x = Math.random();
-            int empStatus = (int) (x * 10) % 3;
+        int dayCount = 1;
+        int workingHours = 0;
+        int totalWage = 0;
+        while (dayCount <= TOTAL_WORKING_DAYS && workingHours <= TOTAL_WORKING_HOURS) {
+            int dailyWage = 0;
+            int empStatus = (int) (Math.random() * 10) % 3;
             switch (empStatus) {
-                case IS_FULLTIME:
-                    System.out.println("Employee worked full time");
+                case IS_FULL_TIME:
+                    System.out.println("Employee worked FULL TIME");
+                    workingHours = workingHours + FULL_DAY_HOUR;
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
                     break;
-                case IS_HALFTIME:
-                    System.out.println("Employee worked part time");
+                case IS_PART_TIME:
+                    System.out.println("Employee worked PART_TIME");
+                    workingHours = workingHours + HALF_DAY_HOUR;
                     dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
                     break;
                 default:
                     System.out.println("Employee is Absent");
             }
-
+            totalWage = totalWage + dailyWage;
             System.out.println("Daily Wage for day : " + dayCount + " is " + dailyWage);
             dayCount++;
         }
-    }
-    }
+        System.out.println("Total Days : "+ (dayCount-1));
+        System.out.println("Total Working Hours : " + workingHours);
+        System.out.println("Total wage for a month :" +totalWage);
 
+    }
+}
 
