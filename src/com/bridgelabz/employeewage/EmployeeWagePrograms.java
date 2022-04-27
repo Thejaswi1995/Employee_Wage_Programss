63664d1ea6221914f532befb312e7ffa1b49c6b3
Thejaswi <thejaswirl187@gmail.com>
2022-04-27 19:14:21 +0530

    Added : Calculation wages per month

--- src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
+++ src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
@@ -5,25 +5,33 @@ public class EmployeeWagePrograms {
     private static final int HALF_DAY_HOUR = 4;
     private static final int IS_FULLTIME = 1;
     private static final int IS_HALFTIME = 2;
+    private static final int TOTAL_WORKING_DAYS = 20;
+
 
     public static void main(String[] args) {
         int dailyWage = 0;
-        double x = Math.random();
-        int empStatus = (int) (x * 10) % 3;
-       switch(empStatus) {
-           case IS_FULLTIME :
-                System.out.println("Employee worked full time");
-                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                break;
-           case IS_HALFTIME :
-                System.out.println("Employee worked part time");
-                dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
-                break;
-           default:
-            System.out.println("Employee is Absent");
-        }
+        int dayCount = 1;
+        while (dayCount <= TOTAL_WORKING_DAYS) {
 
-        System.out.println("Employee Daily Wage :" + dailyWage);
+            double x = Math.random();
+            int empStatus = (int) (x * 10) % 3;
+            switch (empStatus) {
+                case IS_FULLTIME:
+                    System.out.println("Employee worked full time");
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+                case IS_HALFTIME:
+                    System.out.println("Employee worked part time");
+                    dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+
+            System.out.println("Daily Wage for day : " + dayCount + " is " + dailyWage);
+            dayCount++;
+        }
     }
-}
+    }
+
 
