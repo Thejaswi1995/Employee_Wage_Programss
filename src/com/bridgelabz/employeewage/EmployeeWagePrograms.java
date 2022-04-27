2a5182e19b0560576e1bfd7f20a1a9fbcad03e43
Thejaswi <thejaswirl187@gmail.com>
2022-04-27 18:07:23 +0530

    Added : Employee Dailywage-UC_2

--- src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
+++ src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.employeewage;
-public class EmployeeWagePrograms
-{
+public class EmployeeWagePrograms {
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
+        int dailyWage = 0;
         double x = Math.random();
-        int isPresent = (int) (x*10) % 2;
-        if(isPresent==0) {
+        int isPresent = (int) (x * 10) % 2;
+        if (isPresent == 0) {
             System.out.println("Employee is present");
-        }else{
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else {
             System.out.println("Employee is Absent");
         }
 
+        System.out.println("Employee Daily Wage :" + dailyWage);
     }
 }
 
