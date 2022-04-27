8521246f7b4091b6fc666974779272a24bc74ae5
Thejaswi <thejaswirl187@gmail.com>
2022-04-27 17:38:39 +0530

    Added : Welcome Message is added

---
+++ src/com/bridgelabz/employeewage/EmployeeWagePrograms.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewage;
+public class EmployeeWagePrograms
+{
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
